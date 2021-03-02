d4c90963f0e236b446a0e688c52eca9322710bee
sugatakar <sugatakar91@gmail.com>
2021-03-02 19:32:20 +1100

    Second commit

--- src/javaworkpackage/hello.java
+++ src/javaworkpackage/hello.java
@@ -3,7 +3,7 @@ package javaworkpackage;
 public class hello {
 	
 	public static void main(String[] args) {
-		for (int i=1;i<=10;i++)
+		for (int i=1;i<=30;i++)
 		{
 		System.out.println("Hello world "+i);}
 	}
