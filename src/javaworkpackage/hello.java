754b7805e1d45c6d446442f515bc288f2ac62f6c
sugatakar <sugatakar91@gmail.com>
2021-03-02 19:53:32 +1100

    Third commit

--- src/javaworkpackage/hello.java
+++ src/javaworkpackage/hello.java
@@ -5,6 +5,6 @@ public class hello {
 	public static void main(String[] args) {
 		for (int i=1;i<=40;i++)
 		{
-		System.out.println("Hello world "+i);}
+		System.out.println("Sugata is great "+i);}
 	}
 }
