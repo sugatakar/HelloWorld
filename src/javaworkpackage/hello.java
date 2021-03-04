4846f685df7f226d2b6f750ec13c78435266b298
sugatakar <sugatakar91@gmail.com>
2021-03-05 00:01:57 +1100

    sfsfs

--- src/javaworkpackage/hello.java
+++ src/javaworkpackage/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		for (int i=1;i<=40;i++)
 		{
+		System.out.println("hfhf is great "+i);
 		System.out.println("hfhf is great "+i);}
 	}
 }
